438fe123275fe56c150bc59bafba9439a44cfdb9
Austin Song <austinhssong@gmail.com>
2023-11-17 11:37:00 -0500

    basic CRUD todo list completed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import { Task } from "./Task";
 
 function App() {
+  const [todoList, setTodoList] = useState([]);
+  const [newTask, setNewTask] = useState("");
+
+  const handleChange = (e) => {
+    setNewTask(e.target.value);
+  };
+
+  const addTask = () => {
+    const task = {
+      id: todoList.length === 0 ? 1 : todoList[todoList.length - 1].id + 1,
+      taskName: newTask,
+    };
+    setTodoList([...todoList, task]);
+  };
+
+  const deleteTask = (id) => {
+    const newTodoList = todoList.filter((task) => {
+      // if(task === taskName){
+      //   return false
+      // }else{
+      //   return true
+      // }
+      return task.id !== id;
+    });
+    setTodoList(newTodoList);
+  };
+
+  const completeTask = (id) => {
+    setTodoList(
+      todoList.map((task) => {
+        if (task.id === id) {
+          return { ...task, completed: true };
+        } else {
+          return task;
+        }
+      })
+    );
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="addTask">
+        <input onChange={handleChange} />
+        <button onClick={addTask}>Add Task</button>
+      </div>
+      <div className="list">
+        {todoList.map((task) => (
+          <Task
+            taskName={task.taskName}
+            id={task.id}
+            deleteTask={deleteTask}
+            completeTask={completeTask}
+            completed = {task.completed}
+          />
+        ))}
+      </div>
     </div>
   );
 }
